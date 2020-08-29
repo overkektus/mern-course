011200eea909ab6194ddc39f8419077c845ed035
Egor Piskunov <egor.piskunou@gmail.com>
2020-08-29 11:58:49 +0300

    feat(server): add db connection

--- app.ts
+++ app.ts
@@ -1,8 +1,22 @@
 import express from 'express';
 import config from 'config';
+import mongoose, { Error } from 'mongoose';
 
 const app: express.Express = express();
 
 const PORT = config.get('port') || 5000;
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}!!`));
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}!!`));
+  } catch(error) {
+    console.log('Server Error', error.message);
+  }
+}
+
+start();
